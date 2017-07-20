a9f12f453685e882fc9d42947b828f73888f51c8
nakamkaz <nakamkaz@kodain.com>
2017-07-20 23:00:23 +0900

    add a file

---
+++ setenv.bat
@@ -0,0 +1,10 @@
+@echo off
+REM set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_66
+REM set GRADLE_HOME=C:\jdev\gradle-2.2.1
+REM set GROOVY_HOME=C:\jdev\groovy-2.4.6
+set EDITOR="C:\Program Files\Notepad++\notepad++.exe"
+REM set GAEGO="%HOMEDRIVE%%HOMEPATH%\go_appengine"
+set GOPATH=%USERPROFILE%\Go
+set GOROOT=C:\Go
+set PATH=C:\windows\Microsoft.NET\Framework\v4.0.30319;%GOPATH%\bin;%GOROOT%\bin;%PATH%;
+REM set PATH=%GAEGO%;C:\windows\Microsoft.NET\Framework\v4.0.30319;%GOPATH%\bin;%GOROOT%\bin;%PATH%;
